25cff9181fcf8b6149a525b583a4614bfe35ca9f
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-19 22:40:19 +0200

    Update any.js

--- BookStore/frontend/components/any.js
+++ BookStore/frontend/components/any.js
@@ -2,12 +2,7 @@ import React from "react";
 
 const any = () => {
   return (
-    <div>
-      <h1>Any</h1>
-      <p>Any content</p>
-      <button> so ja</button>
-    </div>
-  );
+  
 };
 
 export default any;
