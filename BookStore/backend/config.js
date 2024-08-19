8e1328c8fa19e1b7fd090fd0ad69fb70464c359e
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-19 09:36:06 +0200

    Update config.js

--- BookStore/backend/config.js
+++ BookStore/backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://sabinasandais:samlateef@cluster1.gy3rtt2.mongodb.net/Books-Store?retryWrites=true&w=majority&appName=Cluster1";
