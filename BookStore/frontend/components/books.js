1f34a0d3c965e7366aa033d927db77dab448fc2c
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-19 22:41:49 +0200

    Update books.js

--- BookStore/frontend/components/books.js
+++ BookStore/frontend/components/books.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const books = () => {
-    return <div>
-        <h1>Books</h1>
-        <p>Books will be displayed here
-  </div>;
+  return <div></div>;
 };
 
 export default books;
