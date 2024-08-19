76a46e425fdbafb97cf0b01894e56b08812977a9
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-19 09:36:10 +0200

    Update index.js

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get("/", (req, res) => {
   return res.status(234).send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error);
+  });
