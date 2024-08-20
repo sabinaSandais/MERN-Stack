a704ffd2313d84f6d7355a6660b3fe064f026fc4
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-20 22:15:09 +0200

    Update index.js

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -32,6 +32,16 @@ app.post("/books", async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 });
+//Route for fetching all books
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
