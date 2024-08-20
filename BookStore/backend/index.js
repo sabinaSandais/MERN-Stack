45f1d2c961ed1aa1e504f446edff28f2e2768fde
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-20 12:09:19 +0200

    Update index.js

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -1,12 +1,27 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Hello World");
+});
+//Route for saving a new book
+
+app.post("./books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.bbody.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send({ message: "Send allrequired fields: title,author,publishYear" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
