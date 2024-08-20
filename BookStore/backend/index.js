396fcc3733b6b58d8be22aca224e451105ba634d
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-20 23:52:07 +0200

    Update index.js

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 //Middleware to parse JSON
@@ -11,6 +11,8 @@ app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Hello World");
 });
+
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
