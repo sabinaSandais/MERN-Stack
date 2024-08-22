85aea16d72bdce3a9f25dcaf5758ca40b7c3f5c3
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-22 19:31:29 +0200

    Update ShowBook.jsx

--- BookStore/frontend/src/pages/ShowBook.jsx
+++ BookStore/frontend/src/pages/ShowBook.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import BackButton from "../components/Backbutton";
-
+import Spinner from "../components/Spinner";
 const ShowBook = () => {
-  return (
-    <div>
-      <BackButton />
-      Show Book
-    </div>
-  );
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/bbooks/${id}`)
+      .then((response) => {
+        setBook(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  return <div></div>;
 };
 
 export default ShowBook;
