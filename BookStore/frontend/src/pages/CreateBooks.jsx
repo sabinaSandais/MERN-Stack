1f9f919fb0104146be199a8741df28138f509ce6
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-22 20:16:31 +0200

    Update CreateBooks.jsx

--- BookStore/frontend/src/pages/CreateBooks.jsx
+++ BookStore/frontend/src/pages/CreateBooks.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import BackButton from "../components/Backbutton";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { useNavigate } from "react-router-dom";
 
 const CreateBooks = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios.post("http://localhost:5555/books", data).then(() => {
+      setLoading(false);
+      navigate("/");
+    });
+  };
+
   return <div>Create Books</div>;
 };
-
 export default CreateBooks;
