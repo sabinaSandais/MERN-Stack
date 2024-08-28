58225a9a17e7e0a45723e9b4dd3bcab266c33bac
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-28 11:13:18 +0200

    Update EditBook.jsx

--- BookStore/frontend/src/pages/EditBook.jsx
+++ BookStore/frontend/src/pages/EditBook.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BackButton from "../components/Backbutton";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditBook = () => {
-  return <div></div>;
-};
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((res) => {
+        setTitle(res.data.title);
+        setAuthor(res.data.author);
+        setPublishYear(res.data.publishYear);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("Error while fetching book details");
+        console.log(err);
+      });
+  }, []);
 
-export default EditBook;
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios.post("http://localhost:5555/books", data).then(() => {
+      setLoading(false);
+      navigate("/");
+    });
+  };
+
+ 
