f5fb7979eb0dac6bd5a36bc2b3838055faad15df
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-28 11:17:44 +0200

    Update DeleteBook.jsx

--- BookStore/frontend/src/pages/DeleteBook.jsx
+++ BookStore/frontend/src/pages/DeleteBook.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import BackButton from "../components/Backbutton";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DeleteBook = () => {
   return <div></div>;
